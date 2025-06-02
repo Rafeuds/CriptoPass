c09ebd8a7a27285fbe1e3e5c9461d5ad8024c65f
Rafeuds <rafael.martinssilva@hotmail.com>
2025-06-02 19:45:44 -0300

    Arrumando uns botão

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -6,22 +6,46 @@ import '../Styles/LoginPage.css'
 
 const LoginPage = () => {
 
-    return (
-<div className="login-container">
-  <img src={AppName} />
-  <img src={AppLogo} className='logo-container'/>
+  return (
+    <div className="login-container" style={{ backgroundColor: '#242424' }}>
+      <img src={AppName} />
+      <img src={AppLogo} className='logo-container' />
 
-  <div className="password-container">
-    <input type="password" placeholder="Informe a sua senha" />
-    <img src={EyeClosed} className="password-icon" />
-  </div>
+      <div className="password-container">
 
-  <div className="button-row">
-    <button>Login</button>
-    <button>I</button>
-  </div>
-</div>
-    );
+        <div className='password-row'>
+          <input type="password" placeholder="Informe a sua senha" />
+          <img src={EyeClosed} className="password-icon" />
+        </div>
+
+        <div className="button-row">
+          <button className='button-login'>Login</button>
+          <button className='button-info'>I</button>
+        </div>
+
+      </div>
+      
+    </div>
+
+    /*
+    
+        <div className="login-container" style={{ backgroundColor: '#242424'}}>
+          <img src={AppName} />
+          <img src={AppLogo} className='logo-container' />
+    
+          <div className="password-container">
+            <input type="password" placeholder="Informe a sua senha" />
+            <img src={EyeClosed} className="password-icon" />
+          </div>
+    
+          <div className="button-row">
+            <button>Login</button>
+            <button>I</button>
+          </div>
+        </div>
+    
+    */
+  );
 };
 
 export default LoginPage;
